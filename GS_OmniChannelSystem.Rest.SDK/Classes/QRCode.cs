696aab2240e78d54da7ab0d8961a8ed93bd610b0
Joachim <jdevries@green-solutions.net>
2022-12-19 10:46:48 +0100

    QRCode anpasser

--- GS_OmniChannelSystem.Rest.SDK/Classes/QRCode.cs
+++ GS_OmniChannelSystem.Rest.SDK/Classes/QRCode.cs
@@ -27,10 +27,14 @@ namespace GS.PflanzenCMS.Rest.SDK.Classes
         {
             StringBuilder sb = new StringBuilder();
             sb.Append("0QR");
+            if (info.MemberID != null)
+            {
+                sb.Append("M;");
+                sb.Append(info.MemberID + ";");
+            }
             if (info.Articles != null && info.Articles.Any())
             {
                 sb.Append("A;");
-                sb.Append(info.MemberID + ";");
                 foreach (var article in info.Articles)
                 {
                     if (article != info.Articles.First())
@@ -56,7 +60,7 @@ namespace GS.PflanzenCMS.Rest.SDK.Classes
             // MD5 Hash
             var md5Hasher = MD5.Create();
             // Please replace the salt with the secret salt !!!
-            var data = md5Hasher.ComputeHash(Encoding.Default.GetBytes("salt" + sb.ToString()));
+            var data = md5Hasher.ComputeHash(Encoding.Default.GetBytes("GS74RCJ835" + sb.ToString()));
             var sbMd5 = new StringBuilder();
             for (var i = 0; i < data.Length; i++)
                 sbMd5.Append(data[i].ToString("x2"));
